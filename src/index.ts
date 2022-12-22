f25429ea6dd83dd44a0e90b145f4f1af6efab894
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2022-12-21 21:13:30 -0300

    Cors

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ const app = express()
 
 app.use(express.json())
 app.use(routes);
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(cors({
     origin:"https://brennorodrigues.me/"
 }))
