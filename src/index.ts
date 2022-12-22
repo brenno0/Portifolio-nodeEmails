ce914ab63e9f5293accc3e75290a4e86a7e89a8a
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2022-12-21 21:03:16 -0300

    instalando cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import 'express-async-errors'
 import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
 import dotenv from 'dotenv'
-
+import cors from 'cors'
 const app = express()
+
 
 app.use(express.json())
 app.use(routes);
+app.use(cors({
+    origin:"https://brennorodrigues.me/"    
+}))
 dotenv.config()
 
 app.use((err:Error, request:Request, response:Response, next:NextFunction) => {
@@ -23,5 +27,4 @@ app.use((err:Error, request:Request, response:Response, next:NextFunction) => {
 })
 
 app.listen(process.env.PORT || 3333)
-
 
